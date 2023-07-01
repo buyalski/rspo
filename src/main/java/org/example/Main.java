01596e2565c0bf596a8b9267609f9f8858cbf348
Nikita <n_e_k_1_t@icloud.com>
2023-07-01 13:56:35 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("asd");
+    }
+}
